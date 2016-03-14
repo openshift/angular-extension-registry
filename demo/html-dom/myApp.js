ab363859f5291709be1bd5c395138e38af2cac7f
benjaminapetersen <admin@benjaminapetersen.me>
2016-03-14 16:49:22 -0400

    Readme update

--- demo/html-dom/myApp.js
+++ demo/html-dom/myApp.js
@@ -1,3 +1,5 @@
+'use strict';
+
 angular.module('myapp', [
   'extension-registry'
 ])
@@ -16,18 +18,42 @@ angular.module('myapp', [
 ])
 // arbitrary extension
 .run([
+  '$window',
   'extensionRegistry',
-  function(extensionRegistry) {
+  function($window, extensionRegistry) {
     //dom-nodes
     extensionRegistry.add('dom-nodes', function() {
       return [{
-        type: 'dom',
-        node: '<div> This is html stuff. with directives! <span ng-repeat="item in [1,2,3,4,5]">{{item}},</span></div>'
-      }, {
-        type: 'dom',
-        node: $('<div>')
-                .append('<span>A jQuery node, with directives! <span ng-repeat="item in [1,2,3,4,5]">{{item}},</span>')
-      }];
-    })
+                type: 'dom',
+                node: '<div> This is html stuff. with directives! <span ng-repeat="item in [1,2,3,4,5]">{{item}},</span></div>'
+              }, {
+                type: 'dom',
+                node: $('<div>')
+                        .append('<span>A jQuery node, with directives! <span ng-repeat="item in [1,2,3,4,5]">{{item}},</span>')
+              },{
+                type: 'dom',
+                url: 'http://www.google.com',
+                onClick: function() {
+                  $window.open(this.url, '_blank');
+                },
+                node: [
+                  '<div row ',
+                    'ng-show="item.url" ',
+                    'class="foo" ',
+                    'title="A link title">',
+                      '<div>',
+                        '<i class="fa fa-share" aria-hidden="true"></i>',
+                      '</div>',
+                      '<div>',
+                        '<a ng-click="item.onClick($event)" ',
+                          'ng-href="item.url">',
+                          'Open some link',
+                        '</a>',
+                      '</div>',
+                    '</div>'
+                ].join('')
+              }];
+
+    });
   }
 ]);
